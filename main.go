71fe6c2bb439a06e129a11e82d0edfab40214873
Muhammad Suryono <37969305+MuhammadSuryono@users.noreply.github.com>
2022-05-27 03:00:56 +0700

    Add endpoint submit

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"github.com/MuhammadSuryono/go-helper/db"
 	"github.com/joho/godotenv"
 	"gtihub.com/MuhammadSuryono/cbt-uploader/controllers"
+	"gtihub.com/MuhammadSuryono/cbt-uploader/models/exam"
 )
 
 func main() {
 	_ = godotenv.Load()
 	db.InitConnectionFromEnvironment().CreateNewConnection()
+	_ = db.Connection.AutoMigrate(&exam.ExamResult{})
 
 	server := controllers.RunServer()
 
@@ -17,6 +19,7 @@ func main() {
 	{
 		api.POST("/push", controller.ExamPush)
 		api.GET("/result/:session_key/download", controller.ExportToExcel)
+		api.GET("/submit", controller.CountResultExam)
 	}
 
 	server.Run(":8081")
